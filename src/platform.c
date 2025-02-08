a097a755231973d1fb787887acb0b73166565dd5
rodfer0x80 <rodrigolf080@gmail.com>
2025-02-08 01:34:04 +0000

    [*] fixed top border collision and speed boost while jumping bug

--- src/platform.c
+++ src/platform.c
@@ -6,19 +6,31 @@ int platformCount = 0;
 
 void platformLevel0() {
     platforms[0] = (Platform){
-        (Rectangle){0, SCREEN_HEIGHT / 2 + SAMURAI_FRAME_SIZE / 3, SCREEN_WIDTH, 8},
-        DARKBLUE
+        (Rectangle){0, SCREEN_HEIGHT-8, SCREEN_WIDTH, 8},
+        BLACK
+    };
+    platformCount++;
+    
+    platforms[1] = (Platform){
+        (Rectangle){700, 550, 50, 8},
+        BROWN
     };
     platformCount++;
 
-    platforms[1] = (Platform){
+    platforms[2] = (Platform){
         (Rectangle){200, 400, 300, 8},
         YELLOW
     };
     platformCount++;
 
-    platforms[2] = (Platform){
+    platforms[3] = (Platform){
         (Rectangle){600, 300, 200, 8},
+        GREEN
+    };
+    platformCount++;
+    
+    platforms[4] = (Platform){
+        (Rectangle){600, 700, 200, 8},
         GREEN
     };
     platformCount++;
