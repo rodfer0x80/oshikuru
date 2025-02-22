f9771ec6ea0fd9f0ad3ae45dcacc0bfb17cdf678
rodfer0x80 <rodrigolf080@gmail.com>
2025-02-22 20:09:39 +0000

    Refactor project struct

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
-mkdir -p ./logs
-LOG_FILE="./dev/oshikuru.log"
+LOG_FILE="./oshikuru.log"
 DATE_NOW=$(date -u +"%Y-%m-%dT%H:%M:%S+0000")
 LOG_ENTRY="\n[$DATE_NOW]"
 echo -e "$LOG_ENTRY" >> "$LOG_FILE"
